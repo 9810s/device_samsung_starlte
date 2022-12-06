8dc0d4d503f3d176f3406fbd2e851d6cecc42f60
JeyKul <minkajey@gmail.com>
2022-12-06 15:26:45 +0100

    Revert "aosp --> lineage "
    This reverts commit 12ab2c0beeaa14e9223126a2735b040e81ba08cf.

--- aosp_starlte.mk
+++ aosp_starlte.mk
@@ -25,15 +25,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/samsung/starlte/device.mk)
 
 # Boot Animation
-#TARGET_SCREEN_HEIGHT := 2960
-#TARGET_SCREEN_WIDTH := 1440
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2960
+TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Inherit some common AOSP stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_starlte
+PRODUCT_NAME := aosp_starlte
 PRODUCT_DEVICE := starlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G960F
